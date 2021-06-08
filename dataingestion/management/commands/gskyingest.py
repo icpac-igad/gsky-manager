825f2c6a31a5716a50a1955a46fde9ac350c0cba
Erick Otenyo <otenyo.erick@gmail.com>
2021-06-08 22:34:48 +0300

    Update payload for webhook

--- dataingestion/management/commands/gskyingest.py
+++ dataingestion/management/commands/gskyingest.py
@@ -2,6 +2,7 @@ import codecs
 import hashlib
 import hmac
 import logging
+import time
 
 import requests
 from django.conf import settings
@@ -19,14 +20,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         if GSKY_INGEST_WEBHOOK_URL and WEBHOOK_SECRET:
             logger.info(f"[INGEST COMMAND]: Sending gsky ingest command ")
-
-            payload = {}
+            payload = {"now": time.time()}
             request = requests.Request(
                 'POST', GSKY_INGEST_WEBHOOK_URL,
                 data=payload, headers={})
 
             prepped = request.prepare()
-            signature = hmac.new(codecs.encode(WEBHOOK_SECRET), prepped.body, digestmod=hashlib.sha512)
+            signature = hmac.new(codecs.encode(WEBHOOK_SECRET), prepped.body, digestmod=hashlib.sha256)
             prepped.headers['X-Gsky-Signature'] = signature.hexdigest()
 
             with requests.Session() as session:
