6f7caa72e5250b65b77ea21ca79fa8cdc2e22880
Erick Otenyo <otenyo.erick@gmail.com>
2021-07-07 12:14:20 +0300

    pass optional date to process daily

--- dataingestion/management/commands/processdaily.py
+++ dataingestion/management/commands/processdaily.py
@@ -1,8 +1,8 @@
 import logging
-from datetime import date
+from datetime import date, datetime
 
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from dataingestion.cds import fetch_tcco
 
@@ -10,15 +10,25 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('-d', '--date', help='Date')
 
     def handle(self, *args, **kwargs):
+        data_date = kwargs['date']
+
         # process layers
         logger.info(f"[INGEST COMMAND]: Starting Processing")
 
-        today = date.today().strftime("%Y-%m-%d")
+        if not data_date:
+            data_date = date.today().strftime("%Y-%m-%d")
+        else:
+            try:
+                data_date = datetime.strptime(data_date, "%Y-%m-%d").strftime("%Y-%m-%d")
+            except ValueError:
+                raise CommandError(f"Incorrect date '{data_date}'. Expected Y-M-D")
 
         # fetch carbon monoxide forecast
-        processed = fetch_tcco(today)
+        processed = fetch_tcco(data_date)
 
         if processed:
             call_command('gskyingest')
